8282405659c46b87bf13bcc5ca1fea060db6c739
dsun-hp-windows <sundaniel3@gmail.com>
2019-02-27 12:51:00 -0800

    add feedback linearization script with z and theta controlled.

--- fb_lin.py
+++ fb_lin.py
@@ -13,11 +13,18 @@ import scipy.linalg
 
 
 def lqr(A, B, Q, R):
-    """Solve the continuous time lqr controller.
+    """
+    Solve the continuous time lqr controller.
 
     dx/dt = A x + B u
 
     cost = integral x.T*Q*x + u.T*R*u
+
+    :param A: state matrix
+    :param B: input matrix
+    :param Q: state cost
+    :param R: input cost
+    :return: K, X, eigvals
     """
 
 
@@ -43,19 +50,6 @@ def lpr(expr, prec, **kwargs):
     # latex print with rounding
     print(sp.latex(sp.simplify(round_expr(expr,prec)), symbol_names=symbol_names, **kwargs))
 
-def xprime(M, B, G, x, u):
-    """
-
-    :param M: 6x6 mass matrix
-    :param B: 6x2 matrix for input
-    :param G: 6x1 matrix of gravity
-    :param x: 6x1 current state of system
-    :param u: 2x1 input effort
-    :return: x', the rate of change of the system
-    """
-
-    return np.dot(M,x) + np.dot(B(x[2]), u) + G
-
 ox, oz, ot, m, r, F1, F2, g, v1, v2 = symbols('ox, oz, ot, m, r, F1, F2, g, v1, v2')
 kx, kz, kt, kxd, kzd, ktd = symbols('kx, kz, kt, kxd, kzd, ktd')
 x, z, th, xd, zd, thd = symbols('x z th xd zd thd')
@@ -70,7 +64,6 @@ for k, ending in zip([kx, kz, kt, kxd, kzd, ktd, ], ['x', 'z', 't', '\dot{x}', '
     symbol_names[k] = "k_{" + ending + "}"
 
 qd = sp.Matrix([xd,zd,thd])
-
 D = sp.Matrix(np.diag([m, m, 1/3*m*r**2]))
 u = sp.Matrix([F1, F2])
 v = sp.Matrix([v1,v2])
@@ -86,70 +79,87 @@ subs_dict = {ox : 1e-6, oz : 1e-6, ot : 1e-6, r : .127, m : .1, g:9.8}
 Dinv = sp.Matrix(np.diag([1/m, 1/m, 3/(m*r**2)]))
 qdd = -Dinv@H + Dinv@B@u
 S = np.array([[0,1,0],[0,0,1]])
-ic = np.r_[0, 5, pi / 10, .1, 0, 0]
+ic = np.r_[1, .2, -.1, .1, 5, 0]
 
-f = sp.Matrix([qd,Dinv@H]).subs(subs_dict)
+f = sp.Matrix([qd,-Dinv@H]).subs(subs_dict)
 g_ = sp.Matrix([[0,0],[0,0],[0,0],Dinv@B])
-u = (S@Dinv@B).inv()@(S@Dinv@H + v)
+u_ = ((S@Dinv@B).inv()@(S@Dinv@H + v)).subs(subs_dict)
 
 f = f.subs(subs_dict)
 g_ = g_.subs(subs_dict)
-u = u.subs(subs_dict)
 
 # control design
-A = np.zeros()
+A_ = np.zeros([4,4]); A_[0,1] = 1; A_[2,3] = 1
+B_ = np.zeros([4,2]); B_[1,0] = 1; B_[3,1] = 1
+Q_ = np.diag([1e3,1,1e6,1])
+R_ = np.eye(2)*1e1
+K_, P, eigvals = lqr(A_,B_,Q_,R_)
+Acl = A_-B_@K_
+print(np.linalg.eigvals(Acl))
 
 
-xprime = sp.lambdify((X, v), f+g_@u)
-
+K = sp.zeros(2,6)
+K[:,1] = K_[:,0] #z
+K[:,2] = K_[:,2] #th
+K[:,4] = K_[:,1] #zdot
+K[:,5] = K_[:,3] #thdot
+K = np.array(K).astype(np.float64)
+xprime = sp.lambdify((X, v), f+g_@u_)
 
 # # euler's method
-# dt = .001
-# tfina l = 10
-# times = np.arange(0,tfinal, dt)
-#
-# # simulate
-# x = ic
-# xdes = np.r_[0,5,0,0,0,0]
-# xlist = np.zeros((math.floor(tfinal/dt), 6))
-# ulist = np.zeros((math.floor(tfinal/dt), 2))
-#
-# show_traj = 1
-# show_traj = 0
-#
-# for i in range(len(times)):
-#
-#     u =  np.clip(, Fmin, Fmax)
-#
-#     xlist[i,:] = x
-#     ulist[i,:] = u
-#     x = (f.subs(subs_dict)+g_.subs(subs_dict))*dt + x
-#
-# # make the plot
-# plt.figure(1)
-# plt.subplot(411)
-# for i, l in zip(range(2), ['x','z']):
-#     plt.plot(times, xlist[:,i], label=l)
-# legend = plt.legend(loc='lower right', shadow=True, fontsize='small')
-# plt.xlabel('time')
-# plt.title('position vs. time')
-# plt.subplot(412)
-# plt.plot(times, xlist[:,2], label='theta')
-# plt.xlabel('time')
-# plt.title('theta vs. time')
-# plt.subplot(413)
-# for i, l in zip(range(3,6), ['xdot','zdot','thetadot']):
-#     plt.plot(times, xlist[:,i], label=l)
-# plt.xlabel('time')
-# legend = plt.legend(loc='upper right', shadow=True, fontsize='small')
-# plt.subplot(414)
-# for i, l in zip(range(2), ['F1','F2']):
-#     plt.plot(times, ulist[:,i], label=l)
-# #
-# if show_traj:
-#     plt.figure(2)
-#     plt.plot(xlist[:,0], xlist[:,1], 'b.')
-#     plt.xlabel('x')
-#     plt.ylabel('z')
-#
-# plt.show()
+dt = .001
+tfinal = 5
+
+times = np.arange(0,tfinal, dt)
+Fmin = np.r_[0,0]
+Fmax = np.r_[10,10]
+
+# simulate
+simulate = True
+# simulate = False
+if simulate:
+    x = ic
+    xdes = np.r_[0,0,0,0,0,0]
+    xlist = np.zeros((math.floor(tfinal/dt), 6))
+    ulist = np.zeros((math.floor(tfinal/dt), 2))
+
+    show_traj = 1
+    show_traj = 0
+
+    for i in range(len(times)):
+
+        u = np.clip(-K@x, Fmin, Fmax)
+        xlist[i,:] = x
+        ulist[i,:] = u
+        # print(u)
+        x = xprime(x,u)[:,0]*dt + x
+
+    # make the plot
+    plt.figure(1)
+    plt.subplot(411)
+    for i, l in zip([0,1], ['x','z']):
+        plt.plot(times, xlist[:,i], label=l)
+    legend = plt.legend(loc='lower right', shadow=True, fontsize='small')
+    plt.xlabel('time')
+    plt.title('position vs. time')
+    plt.subplot(412)
+    plt.plot(times, xlist[:,2], label='theta')
+    plt.xlabel('time')
+    plt.title('theta vs. time')
+    plt.subplot(413)
+    for i, l in zip(range(3,6), ['xdot','zdot','thetadot']):
+        plt.plot(times, xlist[:,i], label=l)
+    plt.xlabel('time')
+    legend = plt.legend(loc='upper right', shadow=True, fontsize='small')
+    plt.subplot(414)
+    for i, l in zip(range(2), ['F1','F2']):
+        plt.plot(times, ulist[:,i], label=l)
+    legend=plt.legend(loc='upper right', shadow=True, fontsize='small')
+    #
+    if show_traj:
+        plt.figure(2)
+        plt.plot(xlist[:,1], xlist[:,2], 'b.')
+        plt.xlabel('z')
+        plt.ylabel('$theta$')
+
+    plt.show()
